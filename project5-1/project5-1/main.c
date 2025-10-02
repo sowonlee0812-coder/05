2638c7aecfe74da843c2755bb1180be1c4984abe
sowonlee0812-coder <sowon.lee0812@gmail.com>
2025-10-02 15:25:08 +0900

    project5-2

--- project5-1/project5-1/main.c
+++ project5-1/project5-1/main.c
@@ -11,13 +11,9 @@ int main(int argc, char * argv[])
     scanf("%d", &x);
     
     if (x > 0)
-        printf("양 수 입 니 다.\n");
-    
-    else if (x < 0)
-        printf("음 수 입 니 다.\n");
-    
+        printf("absolute is %i.\n", x);
     else
-        printf("0 입 니 다.\n");
+        printf("absolute is %i.\n", -x);
     
     system("PAUSE");
     return 0;

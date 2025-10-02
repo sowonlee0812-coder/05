565ad22cbccfa2aaeb37f30e725cfbd09fa9bcdd
sowonlee0812-coder <sowon.lee0812@gmail.com>
2025-10-02 15:34:25 +0900

    project5-4

--- project5-1/project5-1/main.c
+++ project5-1/project5-1/main.c
@@ -6,14 +6,17 @@ int main(int argc, char * argv[])
 {
     
     int x;
+    int i;
+    int sum=0;
     
-    printf("Input an integer : ");
+    printf("Input an number : ");
     scanf("%d", &x);
     
-    if (x > 0)
-        printf("absolute is %i.\n", x);
-    else
-        printf("absolute is %i.\n", -x);
+    for(i=1;i<=x;i++)
+        sum = sum + i;
+    
+    printf("sum is %i\n", sum);
+   
     
     system("PAUSE");
     return 0;
